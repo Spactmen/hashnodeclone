7ba248285c9fc77132422561b8d2423b0b8cde01
Siddhi vigneshwar <97656853+Spactmen@users.noreply.github.com>
2022-06-27 19:25:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const menu_btn =  document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav')
+
+menu_btn.addEventListener('click',function(){
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active')
+
+});
+
+
+function menuToggle(){
+  const toggleMenu = document.querySelector(".menu");
+  toggleMenu.classList.toggle('active')
+}
+
